845196b8179c351b71d7621969e21288bfaa40f7
Taher417 <abdullahtaher417@gmail.com>
2025-11-11 07:42:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+let count = 0;
+
+const heartElements = document.getElementsByClassName('heart-bit');
+
+for (let i = 0; i < heartElements.length; i++) {
+    heartElements[i].addEventListener('click', function(e) {
+        e.preventDefault()
+        count++;
+        const heartCounter = document.getElementById('heart-counter');
+        heartCounter.textContent = count;
+    });
+}
+
+
+document.querySelectorAll('.call-btn').forEach(element => {
+    
+ element.addEventListener('click', function(e){
+
+    console.log('call btn clicked')
+})
+});
